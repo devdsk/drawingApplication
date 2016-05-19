07493f1861655a875433bf83a9a2435b1a1b22b1
David Scott Keller <dskinc@outlook.com>
2016-05-19 02:21:31 -0400

    Simple drawing app

---
+++ js/app.js
@@ -0,0 +1,68 @@
+//Problem: No user interaction causes no change to application
+//Solution: When user interacts cause changes appropriately
+var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
+//When clicking on control list items
+$(".controls").on("click", "li", function(){
+  //Deselect sibling elements
+  $(this).siblings().removeClass("selected");
+  //Select clicked element
+  $(this).addClass("selected");
+  //Cache current color
+  color = $(this).css("background-color");
+});
+  
+//When new color is pressed
+$("#revealColorSelect").click(function(){
+  //Show color select or hide the color select
+  changeColor();
+//When color sliders change
+$("#colorSelect").toggle();
+});
+//update the color span
+function changeColor() {
+  var r = $("red").val();
+  var g = $("green").val();
+  var b = $("blue").val();
+  
+  $("#newColor").css("background-color", "rgb(" + r + "," + g +", " + b + ")");
+}
+//When color sliders change
+$("input[type=range]").change(changeColor);  
+//when add color is pressed
+$("addNewColor").click(function() {
+  var $newColor = $("<li></li>");
+  $newColor.css("background-color", $("#newColor").css("background-color"));
+  $(".controls ul").append($newColor);
+    //Select the new color
+  $newColor.click();
+});
+//When the color to the controls ul
+
+//On mouse events on the canvas
+  //var $canvas = $("canvas");
+  $canvas.mousedown(function(e){
+    lastEvent = e;
+    mouseDown = true;
+  }).mousemove(function(e){
+  
+   //Draw lines
+
+  if(mouseDown) {
+    //document.getElementsByTagName("canvas") [0]
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.stroke();
+    //context.closePath()
+    lastEvent = e;
+  }
+  }).mouseup(function(){
+  mouseDown = false;          
+}).mouseleave(function(){
+  $canvas.mouseup();
+  });
